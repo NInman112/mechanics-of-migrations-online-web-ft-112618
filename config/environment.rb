ec58d3ed3f6e8c9c09125b65ebe430deef323eea
Nick Inman <nick@theoatmeal.com>
2019-01-12 21:26:28 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,10 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-DB = {:conn => SQLite3::Database.new("db/artist.db")}
-# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 
 require_relative "../artist.rb"
